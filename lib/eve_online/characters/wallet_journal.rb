553e0d76b16b51c2d54943d752f08537deb70dc6
Igor Zubkov <igor.zubkov@gmail.com>
2016-03-18 19:28:13 +0200

    Add basic EveOnline::Characters::WalletJournal

---
+++ lib/eve_online/characters/wallet_journal.rb
@@ -0,0 +1,25 @@
+module EveOnline
+  module Characters
+    # https://eveonline-third-party-documentation.readthedocs.org/en/latest/xmlapi/char_walletjournal/
+    class WalletJournal < Base
+      API_ENDPOINT = 'https://api.eveonline.com/char/WalletJournal.xml.aspx'.freeze
+
+      attr_reader :key_id, :v_code, :character_id
+
+      def initialize(key_id, v_code, character_id)
+        super()
+        @key_id = key_id
+        @v_code = v_code
+        @character_id = character_id
+      end
+
+      # def entries
+      #
+      # end
+
+      def url
+        "#{ API_ENDPOINT }?keyID=#{ key_id }&vCode=#{ v_code }&characterID=#{ character_id }"
+      end
+    end
+  end
+end
