f6cca8843272197d7a94e34c40fd2fa2ed028988
Ian Flynn <himitsu.monban@gmail.com>
2016-03-27 06:29:27 -0400

    added basic sovereignty query

---
+++ spec/eve_online/sovereignty/campaigns_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe EveOnline::Sovereignty::Campaigns do
+  specify { expect(described_class::API_ENDPOINT).to eq('https://public-crest.eveonline.com/sovereignty/campaigns/') }
+  specify { expect(subject.url).to eq(described_class::API_ENDPOINT) }
+  specify { expect(subject).to be_a(EveOnline::Base) }
+  specify { expect(subject).to be_a(EveOnline::Sovereignty::Campaigns) }
+  specify { expect(subject.parser).to eq(JSON) }
+
+  describe '#initialize' do
+    specify { expect{described_class.new}.to_not raise_error }
+  end
+
+  describe '#results' do
+    let(:response) { double }
+
+    let(:content) { 'some json content' }
+
+    before { expect(response).to receive(:fetch).and_return(content) }
+    before { expect(subject).to receive(:response).and_return(response) }
+
+    specify { expect { subject.result }.not_to raise_error }
+  end
+
+  describe '#response' do
+    let(:parser) { double }
+
+    let(:content) { 'some xml content' }
+
+    before { expect(subject).to receive(:content).and_return(content) }
+
+    before { expect(subject).to receive(:parser).and_return(parser) }
+
+    before { expect(parser).to receive(:parse).with(content) }
+
+    specify { expect { subject.response }.not_to raise_error }
+  end
+
+  describe '#url' do
+    specify { expect(subject.url).to eq(described_class::API_ENDPOINT) }
+  end
+end
+
